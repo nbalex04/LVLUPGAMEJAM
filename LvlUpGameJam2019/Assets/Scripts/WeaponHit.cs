4330b147a6d9707c1075b6032a9c8f8a5575716b
alexandr.favre <alexandre.favre@edu.hefr.ch>
2019-02-15 13:21:52 +0100

    Stats + catch fish

---
+++ LvlUpGameJam2019/Assets/Scripts/WeaponHit.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class WeaponHit : MonoBehaviour {
+
+    public Inventary inventary;
+
+    void OnCollisionEnter2D(Collision2D col)
+    { 
+        if (col.gameObject.tag == "BlueFish")
+        {
+            inventary.nbBlueFish++;
+        }
+        else if (col.gameObject.tag == "GreenFish")
+        {
+            inventary.nbGreenFish++;
+        }
+        else if (col.gameObject.tag == "RedFish")
+        {
+            inventary.nbRedFish++;
+        }
+        else if (col.gameObject.tag == "YellowFish")
+        {
+            inventary.nbYellowFish++;
+        }
+        Destroy(col.gameObject);
+    }
+}
